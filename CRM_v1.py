f745c1c7d2ee1cec15b47f368d3d103ea0cd9d13
GinormousSalmon <terleczcky.alex@gmail.com>
2020-04-28 23:31:15 +0300

    added qt threading to realtime updating chat messages chat working

--- CRM_v1.py
+++ CRM_v1.py
@@ -1,8 +1,13 @@
-from PyQt5 import uic
+import traceback
+
+from PyQt5 import uic, QtCore, Qt
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 import sys
 import zmq
 import os
+from datetime import datetime
+import time
 
 
 def resource_path(relative_path):
@@ -16,9 +21,42 @@ context = zmq.Context()
 socket = context.socket(zmq.REQ)
 result = socket.connect("tcp://45.143.136.117:9000")
 
+messages = ""  # local chat history
+update = False
+
 # loading ui's
 form_main, base_main = uic.loadUiType(resource_path('mainForm.ui'))
 form_chat, base_chat = uic.loadUiType(resource_path('chat.ui'))
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    # import traceback
+    text += ''.join(traceback.format_tb(tb))
+
+    print(text)
+    Qt.QMessageBox.critical(None, 'Error', text)
+    quit()
+
+
+sys.excepthook = log_uncaught_exceptions
+
+
+class AThread(QThread):
+    threadSignalAThread = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        global messages, update
+        while update:
+            socket.send_string("history")
+            print("waiting answer from server", datetime.now())
+            messages = str(socket.recv_string())  # download chat history
+            print(messages)
+            self.threadSignalAThread.emit(messages)
+            QThread.msleep(1000)
 
 
 class MainUI(base_main, form_main):
@@ -75,9 +113,11 @@ class MainUI(base_main, form_main):
 
 class ChatUI(base_chat, form_chat):
     def __init__(self):
+        global update, messages
         super(base_chat, self).__init__()
         self.setupUi(self)
         self.main = None
+        self.thread = None
 
         # widgets binding
         self.back_button = self.findChild(QPushButton, 'backFromChatButton')
@@ -90,12 +130,64 @@ class ChatUI(base_chat, form_chat):
         self.message_field.returnPressed.connect(self.send_button_click)
 
         self.chat_field = self.findChild(QTextEdit, 'chatField')
-
         self.chat_field.setHtml(messages)  # first updating chat field
         self.chat_field.verticalScrollBar().setValue(self.chat_field.verticalScrollBar().maximum())  # scroll to end
 
+        update = True
+
+        # ----------------------------------------------------------------------------------------------------
+        # my_thread = threading.Thread(target=self.chat_updater)
+        # my_thread.daemon = True
+        # my_thread.start()
+        self.using_q_thread()
+
+    def using_q_thread(self):
+        if self.thread is None:
+            self.thread = AThread()
+            self.thread.threadSignalAThread.connect(self.on_threadSignalAThread)
+            self.thread.finished.connect(self.finishedAThread)
+            self.thread.start()
+            # self.btnA.setText("Stop AThread(QThread)")
+        else:
+            self.thread.terminate()
+            self.thread = None
+            # self.btnA.setText("Start AThread(QThread)")
+
+    def finishedAThread(self):
+        self.thread = None
+        # self.btnA.setText("Start AThread(QThread)")
+
+    def on_threadSignalAThread(self, value):
+        # self.msg.label.setText(str(value))
+        # print(value)
+        self.chat_field.setHtml(str(value))  # updating chat field
+        self.chat_field.verticalScrollBar().setValue(self.chat_field.verticalScrollBar().maximum())  # scroll to end
+        # Восстанавливаем визуализацию потокового окна, если его закрыли. Поток работает.
+        # .setVisible(true) или .show() устанавливает виджет в видимое состояние,
+        # если видны все его родительские виджеты до окна.
+        # if not self.msg.isVisible():
+        #     self.msg.show()
+
+    print("started")
+
+    # ----------------------------------------------------------------------------------------------------
+
+    def chat_updater(self):
+        global messages, update
+        while update:
+            socket.send_string("history")
+            print("waiting answer from server")
+            messages = str(socket.recv_string())  # download chat history
+            print(messages)
+            self.chat_field.setHtml("messages")  # updating chat field
+            self.chat_field.verticalScrollBar().setValue(self.chat_field.verticalScrollBar().maximum())  # scroll to end
+            time.sleep(1)
+
     def back_from_chat_click(self):
         # go back to main window
+        self.thread.terminate()
+        self.thread = None
+
         self.main = MainUI()
         self.main.show()
         self.close()
@@ -108,125 +200,12 @@ class ChatUI(base_chat, form_chat):
             messages += text + '<br/>'  # appending message to local history
             socket.send_string("in_mes|" + text)
             print(str(socket.recv_string()))
-            self.chat_field.setHtml(messages)  # updating chat field
-            self.chat_field.verticalScrollBar().setValue(self.chat_field.verticalScrollBar().maximum())  # scroll to end
+        self.chat_field.setHtml(messages)  # updating chat field
+        self.chat_field.verticalScrollBar().setValue(self.chat_field.verticalScrollBar().maximum())  # scroll to end
         self.message_field.clear()
 
-
-socket.send_string("history")
-print("waiting answer from server")
-messages = str(socket.recv_string())  # download chat history
 
 app = QApplication(sys.argv)
 window = MainUI()
 window.show()
 sys.exit(app.exec())
-
-# class ChatUI(QWidget):
-#     def __init__(self, parent=None):
-#         super(ChatUI, self).__init__()
-#         uic.loadUi('chat.ui', self)
-#         # mainwindow.setWindowIcon(QtGui.QIcon('PhotoIcon.png'))
-#         # self.ToolsBTN = QPushButton('text', self)
-#         # self.ToolsBTN.move(50, 350)
-#
-#
-# class Ui(QMainWindow):
-#     def __init__(self):
-#         super(Ui, self).__init__()
-#         uic.loadUi('mainForm.ui', self)
-#
-#         self.crm_button = self.findChild(QPushButton, 'crmButton')
-#         self.crm_button.clicked.connect(self.crm_button_click)
-#
-#         self.hr_button = self.findChild(QPushButton, 'hrHelperButton')
-#         self.hr_button.clicked.connect(self.hr_button_click)
-#
-#         self.equipment_button = self.findChild(QPushButton, 'equipmentStateButton')
-#         self.equipment_button.clicked.connect(self.equipment_button_click)
-#
-#         self.staff_button = self.findChild(QPushButton, 'staffButton')
-#         self.staff_button.clicked.connect(self.staff_button_click)
-#
-#         self.store_button = self.findChild(QPushButton, 'storeButton')
-#         self.store_button.clicked.connect(self.store_button_click)
-#
-#         self.chat_button = self.findChild(QPushButton, 'chatButton')
-#         self.chat_button.clicked.connect(self.chat_button_click)
-#
-#         self.main_layout = self.findChild(QLayout, 'mainLayout')
-#         self.chat_layout = self.findChild(QLayout, 'chatLayout')
-#
-#         self.main_widget = self.findChild(QWidget, 'widget1')
-#         self.chat_widget = self.findChild(QWidget, 'widget2')
-#
-#         print(self.chat_layout)
-#
-#         # self.chat_field = self.findChild(QTextEdit, 'chatField')
-#         # self.chat_field.setVisible(False)
-#         #
-#         # self.input_message = self.findChild(QLineEdit, 'inputMessage')
-#         # self.input_message.hide()
-#         self.show()
-#
-#     def start_chat_ui(self):
-#         # self.setLayout(self.main_layout)
-#         stack = QStackedLayout()
-#         # stack.addWidget(self.chat_widget)
-#         # stack.setCurrentWidget(self.chat_widget)
-#         self.setLayout(self.chat_layout)
-#         self.show()
-#
-#         # self.ChatUI = ChatUI(self)
-#         # self.setWindowTitle("Chat")
-#         # self.setCentralWidget(self.ChatUI)
-#         # # self.send_button = self.findChild(QPushButton, 'sendButton')
-#         # # self.send_button.clicked.connect(self.crm_button_click)
-#         # self.show()
-#
-#     def crm_button_click(self):
-#         # This is executed when the button is pressed
-#         print('crm_button_click')
-#         uic.loadUi('mainForm.ui', self)
-#         self.show()
-#         # self.setLayout(self.chat_layout)
-#         # self.show()
-#         # self.start_chat_ui()
-#         # self.crm_button.hide()
-#
-#     def hr_button_click(self):
-#         # This is executed when the button is pressed
-#         print('hr_button_click')
-#         uic.loadUi('chat.ui', self)
-#         self.show()
-#
-#     def equipment_button_click(self):
-#         # This is executed when the button is pressed
-#         print('equipment_button_click')
-#
-#     def staff_button_click(self):
-#         # This is executed when the button is pressed
-#         print('staff_button_click')
-#         # self.crm_button.show()
-#
-#     def store_button_click(self):
-#         # This is executed when the button is pressed
-#         print('store_button_click')
-#         # self.startUIToolTab()
-#
-#     def chat_button_click(self):
-#         # This is executed when the button is pressed
-#         print('chat_button_click')
-#         self.start_chat_ui()
-#
-#
-# class Ui2(QMainWindow):
-#     def __init__(self, ui):
-#         super(Ui, self).__init__()
-#         uic.loadUi(ui, self)
-#         self.show()
-#
-#
-# app = QApplication(sys.argv)
-# window = Ui()
-# sys.exit(app.exec())
